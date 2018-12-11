56cbd865d8effc511c52fcd0c92e1a5a241ffb7b
Blake Haddad <bhaddad1995@gmail.com>
2018-12-10 20:06:54 -0700

    Add infile.dat reading

--- linkStateRouting.java
+++ linkStateRouting.java
@@ -1,4 +1,9 @@
 import java.util.Map;
+import java.util.Vector;
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+import java.text.*;
 
 // Blake Haddad & Andy Nguyen
 // CS-570
@@ -8,8 +13,95 @@ import java.util.*;
 
 public class linkStateRouting{
 
-    public static void main(String[] args){
+    public void loadFile(){
+        HashMap<Integer, String> IDandNetName = new HashMap<>();
+        ArrayList<Integer> IDindex = new ArrayList<>();
+        String[] splitID;
+        String[] splitLink;
+        String splitnetName;
 
+        ArrayList<ArrayList<Integer>> linkedIDs = new ArrayList<>();
+        LinkedList<Integer> linkCosts = new LinkedList<Integer>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        int cost;
+        int routerID;
+        int i = 0;
+        int count =0;
+
+
+
+        try(BufferedReader br = new BufferedReader(new FileReader("infile.dat"))) {
+            for(String line; (line = br.readLine()) != null; ) {
+                if(line.equals("")){
+                    break;
+                }
+
+                if (line.charAt(0) != ' ') {
+                    splitID = line.split(" ");
+                    routerID= Integer.parseInt(splitID[0]);
+                    IDindex.add(routerID);
+                    splitnetName = splitID[1];
+                    IDandNetName.put(routerID, splitnetName);
+                    if (count > 0) {
+                        ArrayList<Integer> copiedTemp = new ArrayList<Integer>();
+                        for(Integer nt : temp){
+                            copiedTemp.add(new Integer(nt));
+                        }
+                        System.out.println("temp: " + temp);
+                        linkedIDs.add(copiedTemp);
+                        System.out.println("Being cleared");
+                        temp.clear();
+                    }
+                    count++;
+
+                }
+
+                if(line.charAt(0) == ' '){
+                    splitLink = line.split(" ", 2);
+                    temp.add(Integer.parseInt(splitLink[1]));
+                    System.out.println("Being added to temp: " + Integer.parseInt(splitLink[1]));
+                }
+
+                for (int o = 0; o < temp.size(); o++){
+//                    System.out.println(temp.get(o));
+
+                }
+
+
+
+                i++;
+
+
+            }
+        } catch (IOException j) {
+            System.err.print("Error loading file with exception:\n"+j);
+            j.printStackTrace();
+            System.exit(0);
+        }
+
+        for (Integer ID : IDandNetName.keySet())
+        {
+            Integer IDKey = ID;
+            String networkName = IDandNetName.get(IDKey);
+
+            System.out.println("ID: " + IDKey);
+            System.out.println("Network Name: " + networkName);
+        }
+
+        for (int k = 0; k < linkedIDs.size(); k++){
+            System.out.println("ID: " + k);
+            System.out.println(linkedIDs.get(k));
+        }
+    }
+
+    public void printAll()
+    {
+
+    }
+
+    public static void main(String[] args){
+        linkStateRouting test = new linkStateRouting();
+        test.loadFile();
     }
 
 }
